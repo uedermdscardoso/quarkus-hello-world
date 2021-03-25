7e1fc93471cc3c309a183f6ad377006aba5efbc2
Unknown <ueder.mds.cardoso@gmail.com>
2021-03-24 23:00:30 -0300

    add quarkus

---
+++ hello-world/src/test/java/dev/uedercardoso/hello/world/NativeHelloWorldResourceIT.java
@@ -0,0 +1,9 @@
+package dev.uedercardoso.hello.world;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloWorldResourceIT extends HelloWorldResourceTest {
+
+    // Execute the same tests but in native mode.
+}
